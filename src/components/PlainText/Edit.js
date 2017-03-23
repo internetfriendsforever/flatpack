b3f1852b2c3c40a56ccb841d7cc57536405f3f02
s-g-k <sebastian.kalvik@gmail.com>
2017-03-23 12:34:08 +0100

    change {value} for textarea on PlainText component

--- src/components/PlainText/Edit.js
+++ src/components/PlainText/Edit.js
@@ -34,7 +34,7 @@ class EditPlainText extends React.Component {
   }
 
   onChange = (e) => {
-    this.props.setValue(e.target.value)
+    this.props.setValue(JSON.stringify(e.target.value))
   }
 
   componentDidMount () {
@@ -52,9 +52,8 @@ class EditPlainText extends React.Component {
             placeholder={placeholder}
             onChange={::this.onChange}
             ref='textarea'
-          >
-            {value}
-          </textarea>
+            value={value}
+          />
         </span>
       </EditIndicator>
     )
