fc2895ad0ee15eb63fcffdcb6a9f6278816927b9
Daniel Mahal <danielmahal@gmail.com>
2017-11-14 23:01:14 +0100

    Font size

--- src/ui/Root.js
+++ src/ui/Root.js
@@ -2,5 +2,5 @@ import styled from 'styled-components'
 
 export default styled.div`
   font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
-  font-size: 16;
+  font-size: 14;
 `
